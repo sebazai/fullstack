09f0f335eee95d77ca3bc831f0c2405b72f3fa97
sebazai <sebastian.sergelius@helsinki.fi>
2018-07-13 16:19:45 +0300

    tehtavat 1.6-1.11

--- osa1/osa1-tehtava-1.6-1.11-unicafe/src/index.js
+++ osa1/osa1-tehtava-1.6-1.11-unicafe/src/index.js
@@ -13,72 +13,17 @@ class App extends React.Component {
       }
     }
 
-    asetaArvoHyva = (arvo) => {
+    pressButton = (button) => {
         return () => {
-            this.setState({ hyva : arvo })
-        }
-    }
-    asetaArvoNeutraali = (arvo) => {
-        return () => {
-            this.setState({ neutraali : arvo })
-        }
-    }
-    asetaArvoHuono = (arvo) => {
-        return () => {
-            this.setState({ huono : arvo })
+            this.setState({ [button] : this.state[button] +1 })
         }
     }
 
-    statistic = () => {
-        const keskiarvo = this.state.hyva - this.state.huono / (this.state.hyva+this.state.neutraali+this.state.huono)
-        return (
-            <div>
-            keskiarvo {keskiarvo}<br />
-            </div>
-        )
-    }
-    
-  
-    render() { 
-    const Button = ({handleClick, text}) => (
-        <button onClick={handleClick}>
-            {text}
-        </button>
-    )
-
-    const Statistics = () => {
-        if (this.state.hyva === 0 && this.state.neutraali === 0 && this.state.huono === 0) {
-            return (
-                <div>
-                <h1>statistiikka</h1>
-                <em>ei yhtään palautetta annettu</em>
-                </div>
-            )
-        }
-        return (
-            <div>
-                <h1>statistiikka</h1>
-                <table>
-                <tr>
-                <td>hyvä</td><td>{this.state.hyva}</td>
-                </tr>
-                <tr>
-                <td>neturaali</td><td>{this.state.neutraali}</td>
-                </tr>
-                <tr>
-                <td>huono</td><td>{this.state.huono}</td>
-                </tr>
-                {Statistic()}
-                </table>
-            </div>
-        )
-    }
-
-    const Statistic = () => {
+    Statistic = () => {
         const kaikki = this.state.hyva+this.state.neutraali+this.state.huono
         const arvo = this.state.hyva-this.state.huono
-        const keskiarvo = arvo / kaikki
-        const positiivi = this.state.hyva/kaikki*100
+        let keskiarvo = (arvo / kaikki).toFixed(1)
+        const positiivi = (this.state.hyva/kaikki*100).toFixed(1)
         return (
             <tbody>
             <tr>
@@ -91,22 +36,60 @@ class App extends React.Component {
         )
     }
     
+    
+    render() { 
+    const Button = ({handleClick, text}) => (
+        <button onClick={handleClick}>
+            {text}
+        </button>
+    )
+
+    const Statistics = () => {
+        if (this.state.hyva === 0 && this.state.neutraali === 0 && this.state.huono === 0) {
+            return (
+                <div>
+                <em>ei yhtään palautetta annettu</em>
+                </div>
+            )
+        }
+        return (
+            <div>
+                <table>
+                <tr>
+                    <td>hyvä</td>
+                    <td>{this.state.hyva}</td>
+                </tr>
+                <tr>
+                    <td>neturaali</td>
+                    <td>{this.state.neutraali}</td>
+                </tr>
+                <tr>
+                    <td>huono</td>
+                    <td>{this.state.huono}</td>
+                </tr>
+                    {this.Statistic()}
+                </table>
+            </div>
+        )
+    }
+
       return (
         <div>
           <div>
             <h1>anna palautetta</h1>
             <Button 
-                handleClick={this.asetaArvoHyva(this.state.hyva + 1)}
+                handleClick={this.pressButton("hyva")}
                 text="hyva"
             />
             <Button 
-                handleClick={this.asetaArvoNeutraali(this.state.neutraali + 1)}
+                handleClick={this.pressButton("neutraali")}
                 text="neutraali"
             />
             <Button 
-                handleClick={this.asetaArvoHuono(this.state.huono + 1)}
+                handleClick={this.pressButton("huono")}
                 text="huono"
             />
+            <h1>statistiikka</h1>
             <Statistics />
           </div>
         </div>
