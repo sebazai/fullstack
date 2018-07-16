aee703e6e08d693b97c08b4069bb9b1c75054dce
sebazai <sebastian.sergelius@helsinki.fi>
2018-07-16 10:26:55 +0300

    lomakemuutokset

--- osa2/src/App.js
+++ osa2/src/App.js
@@ -1,16 +1,71 @@
 import React from 'react'
 import Note from './components/Note'
 
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      notes: props.notes,
+      newNote: 'uusi muistiinpano...',
+      showAll: true
+    }
+  }
+  addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: this.state.newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: this.state.notes.length + 1
+    }
 
-const App = ({notes}) => {
-  return (
-    <div>
-      <h1>Muistiinpanot</h1>
-      <ul>
-        {notes.map(note=><Note key={note.id} note={note} />)}
-      </ul>
-    </div>
-  )
+    const notes = this.state.notes.concat(noteObject)
+
+    this.setState({
+      notes: notes,
+      newNote: ''
+    })
+    console.log('nappia painettu')
+  }
+
+  handleNoteChange = (event) => {
+    console.log(event.target.value)
+    this.setState({newNote: event.target.value})
+  }
+
+  toggleVisible = () => {
+    this.setState({showAll : !this.state.showAll})
+  }
+
+  render() {
+    const notesToShow = 
+      this.state.showAll ?
+        this.state.notes :
+        this.state.notes.filter(note => note.important === true)
+
+    const label = this.state.showAll ? 'vain tärkeät' : 'kaikki'
+
+    return (
+      <div>
+        <h1>Muistiinpanot</h1>
+        <div>
+          <button onClick={this.toggleVisible}>
+            näytä {label}
+          </button>
+        </div>
+        <ul>
+          {notesToShow.map(note=><Note key={note.id} note={note} />)}
+        </ul>
+        <form onSubmit={this.addNote}>
+          <input 
+            value={this.state.newNote} 
+            onChange={this.handleNoteChange} 
+          />
+          <button type="submit">tallenna</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App
