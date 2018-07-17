a48fb5b5623cc9a33ae08fbd4201aff36c09770c
sebazai <sebastian.sergelius@helsinki.fi>
2018-07-17 12:17:41 +0300

    korjattu warningit pois

--- osa2/osa2-tehtava-2.12-2.13-maidentiedot/src/index.js
+++ osa2/osa2-tehtava-2.12-2.13-maidentiedot/src/index.js
@@ -1,8 +1,6 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import App from './App'
-import axios from 'axios'
-import Country from './components/Country'
 
 ReactDOM.render(
     <App />,
