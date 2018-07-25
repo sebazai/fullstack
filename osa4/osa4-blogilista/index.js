f6c0f58428759c8ebbf34fbee331b994607ceb69
sebazai <sebastian.sergelius@helsinki.fi>
2018-07-25 10:04:19 +0300

    tehtavat 4.1-4.2

--- osa4/osa4-blogilista/index.js
+++ osa4/osa4-blogilista/index.js
@@ -4,39 +4,18 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const Blog = require('./models/blog.js')
+const blogsRouter = require('./controllers/blogs')
 
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-module.exports = Blog
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use('/api/blogs', blogsRouter)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
 
 const PORT = 3003
 app.listen(PORT, () => {
